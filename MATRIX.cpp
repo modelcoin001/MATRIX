dd02fe5dd087e2421989616ea46f2311e1acbf2d
KlimovMark <klimov.mark87@gmail.com>
2020-06-05 00:10:53 +0700

    ver0.2 renamed program file, added matrix.cpp with methods of class Matrix

--- MATRIX.cpp
+++ MATRIX.cpp
@@ -1,63 +1,164 @@
-п»ї#include <iostream>
+
 #include "matrix.h"
-using namespace std;
+#include <iostream>
 
-int main()
+Matrix::Matrix() //конструктор по умолчанию. Возвращает пустую матрицу
 {
-    setlocale(LC_ALL, "Russian");
-    int x = 2;
-    int y = 3;
-    Matrix M(x, y);
-    for (int i = 0; i < x; i++)
+    m = 0;
+    n = 0;
+    a = NULL;
+}
+
+Matrix::Matrix(int m1, int n1)//конструктор с двумя параметрами - шириной и высотой
+{
+    m = m1;
+    n = n1;
+    a = new double* [m];
+    for (int i = 0; i < m; i++)
+        a[i] = new double[n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = 0;
+}
+
+/*Matrix::~Matrix()//деструктор
+ {
+     for (int i = 0; i < m; i++)
+             delete[] a[i];
+         delete[] a;
+}*/
+
+
+
+double Matrix::getMij(int x, int y)
+{
+    return a[x][y];
+}
+
+void Matrix::setMij(int i1, int j1, double x)//записать в элемент матрицы с координатами [i1] [j1] значение x
+{
+    a[i1][j1] = x;
+}
+
+void Matrix::print()//вывод матрицы на экран
+{
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < y; j++)
+        for (int j = 0; j < n; j++)
         {
-            M.setMij(i, j, double(i) + double(j));
+            std::cout << a[i][j] << "\t";
         }
+        std::cout << std::endl;
     }
+}
 
-    Matrix N(x, y);
-    for (int i = 0; i < x; i++)
+int Matrix::getSizeX()//число строк
+{
+    return m;
+}
+
+int Matrix::getSizeY()//число столбцов
+{
+    return n;
+}
+
+Matrix operator+(const Matrix& left, const Matrix& right)
+{
+
+    if (left.m == right.m && right.n == left.n)
     {
-        for (int j = 0; j < y; j++)
-        {
-            N.setMij(i, j, 2*(double(i) + double(j)));
-        }
+        Matrix result(left.m, left.n);
+        for (int i = 0; i < result.m; i++)
+            for (int j = 0; j < result.n; j++)
+                result.a[i][j] = left.a[i][j] + right.a[i][j];
+        return result;
     }
+    else
+    {
+        std::cout << "Матрицы разного размера";
+        exit(1);
+    }
+}
 
- 
-    Matrix R(y, x);
-    R = M + N;
-    cout << "РњР°С‚СЂРёС†Р° M" <<endl ;
-    M.print();
-    cout << endl;
+Matrix operator-(const Matrix& left, const Matrix& right)
+{
 
-    cout << "РњР°С‚СЂРёС†Р° N" << endl;
-    N.print();
-    cout << endl;
+    if (left.m == right.m && right.n == left.n)
+    {
+        Matrix result(left.m, left.n);
+        for (int i = 0; i < result.m; i++)
+            for (int j = 0; j < result.n; j++)
+                result.a[i][j] = left.a[i][j] - right.a[i][j];
+        return result;
+    }
+    else
+    {
+        std::cout << "Матрицы разного размера";
+        exit(1);
+    }
+}
 
-    cout << "РњР°С‚СЂРёС†Р° R = M + N" << endl;
-    R.print();
-    cout << endl;
+Matrix operator*(const Matrix& left, const double right)
+{
+    Matrix result(left.m, left.n);
+    for (int i = 0; i < left.m; i++)
+        for (int j = 0; j < left.n; j++)
+            result.a[i][j] = left.a[i][j] * right;
+    return result;
+}
 
-    R = M - N;
-    cout << "РњР°С‚СЂРёС†Р° R = M - N" << endl;
-    R.print();
-    cout << endl;
+Matrix operator*(const Matrix& left, const Matrix& right)
+{
+    if (left.n == right.m)
+    {
+        Matrix result(left.m, right.n);
+        for (int i = 0; i < left.m; i++)
+            for (int j = 0; j < right.n; j++)
+                for (int k = 0; k < right.n; k++)
+                    result.a[i][j] += (left.a[i][k] * right.a[k][j]);
+        return result;
+    }
+    else
 
-    R = M * 2;
-    cout << "РњР°С‚СЂРёС†Р° R = M * 2" << endl;
-    R.print();
-    cout << endl;
+        std::cout << "Матрицы таких размеров перемножать нельзя";
+}
 
-    R = !N;
-    cout << "РўСЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅР°СЏ РјР°С‚СЂРёС†Р° N" << endl;
-    R.print();
-    cout << endl;
+Matrix operator!(const Matrix& M)
+{
+    Matrix result(M.n, M.m);
+    for (int i = 0; i < M.m; i++)
+        for (int j = 0; j < M.n; j++)
+            result.a[j][i] = M.a[i][j];
+    return result;
+}
 
-    Matrix A(x, y);
-    A = !N;
-    R = M * A;
-    cout << "РџСЂРѕРёР·РІРµРґРµРЅРёРµ РјР°С‚СЂРёС†С‹ M Рё С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅРѕР№ РјР°С‚СЂРёС†С‹ N" << endl;
-    R.print();
+Matrix& Matrix::operator=(const Matrix& right)
+{
+    if (n != right.n || m != right.m)
+    {
+        for (int i = 0; i < m; i++)
+            delete[] a[i];
+        delete[] a;
+        n = right.n;
+        m = right.m;
+        a = new double* [m];
+        for (int i = 0; i < m; i++)
+            a[i] = new double[n];
+    }
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = right.a[i][j];
+    return *this;
+}
+
+void print(Matrix M)
+{
+    for (int i = 0; i < M.getSizeX(); i++)
+    {
+        for (int j = 0; j < M.getSizeY(); j++)
+        {
+            std::cout << M.getMij(i, j) << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
---
+++ program.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include "matrix.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x = 2;
+    int y = 3;
+    Matrix M(x, y);
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            M.setMij(i, j, double(i) + double(j));
+        }
+    }
+
+    Matrix N(x, y);
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            N.setMij(i, j, 2*(double(i) + double(j)));
+        }
+    }
+
+ 
+    Matrix R(y, x);
+    R = M + N;
+    cout << "Матрица M" <<endl ;
+    M.print();
+    cout << endl;
+
+    cout << "Матрица N" << endl;
+    N.print();
+    cout << endl;
+
+    cout << "Матрица R = M + N" << endl;
+    R.print();
+    cout << endl;
+
+    R = M - N;
+    cout << "Матрица R = M - N" << endl;
+    R.print();
+    cout << endl;
+
+    R = M * 2;
+    cout << "Матрица R = M * 2" << endl;
+    R.print();
+    cout << endl;
+
+    R = !N;
+    cout << "Транспонированная матрица N" << endl;
+    R.print();
+    cout << endl;
+
+    Matrix A(x, y);
+    A = !N;
+    R = M * A;
+    cout << "Произведение матрицы M и транспонированной матрицы N" << endl;
+    R.print();
+}
